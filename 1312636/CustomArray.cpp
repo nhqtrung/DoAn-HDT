e9e27dbf6e5a5c82c1f37bff5a33d132c77d8776
1312636 <nhqtrung1995@gmail.com>
2018-11-15 17:29:04 +0700

    first class -insert method

--- 1312636/CustomArray.cpp
+++ 1312636/CustomArray.cpp
@@ -2,44 +2,40 @@
 #include "CustomArray.h"
 
 template <class T>
-CustomArray<T>::CustomArray(int m_N)
-{
+CustomArray<T>::CustomArray(int m_N) {
 	this->N = m_N;
 	this->arr = new T[3 * this->N];
-	this->lowerBound = &arr[0];
-	this->begin = &arr[this->N];
-	this->end = &arr[2 * this->N];
-	this->upperBound = &arr[3 * this->N - 1];
+	this->p_LowerBound = &arr[0];
+	this->p_Begin = &arr[this->N];
+	this->p_End = &arr[2 * this->N];
+	this->p_UpperBound = &arr[3 * this->N - 1];
 }
 
 template <class T>
-CustomArray<T>::CustomArray(int m_N, T defaultValue)
-{
-	this->N = m_N;
-	this->arr = new T[3 * this->N];
-	this->lowerBound = &arr[0];
-	this->begin = &arr[this->N];
-	this->end = &arr[2 * this->N];
-	this->upperBound = &arr[3 * this->N - 1];
+CustomArray<T>::CustomArray(int m_N, T defaultValue) {
+	N = m_N;
+	arr = new T[3 * this->N];
+	this->p_LowerBound = &arr[0];
+	this->p_Begin = &arr[this->N];
+	this->p_End = &arr[2 * this->N];
+	this->p_UpperBound = &arr[3 * this->N - 1];
 	for (int i = 0; i < this->N; i++)
 	{
-		arr[this->N + i] = defaultValue;
+		*(arr + N + i) = defaultValue;
 	}
 }
 
 template <class T>
-void CustomArray<T>::printArr()
-{
+void CustomArray<T>::printArr() {
 	for (int i = 0; i < this->N; i++)
 	{
-		std::cout << arr[this->N + i];
+		std::cout << *(arr + N + i);
 	}
 	std::cout << " " << std::endl;
 }
 
 template <class T>
-void CustomArray<T>::setDefaultValue(T defaultValue)
-{
+void CustomArray<T>::setDefaultValue(T defaultValue) {
 	for (int i = 0; i < this->N; i++)
 	{
 		arr[this->N + i] = defaultValue;
@@ -47,8 +43,7 @@ void CustomArray<T>::setDefaultValue(T defaultValue)
 }
 
 template <class T>
-CustomArray<T>::CustomArray()
-{
+CustomArray<T>::CustomArray() {
 
 }
 
@@ -57,18 +52,179 @@ CustomArray<T>::~CustomArray()
 {
 }
 
+
+/* Copy constructor */
 template <class T>
-CustomArray<T>::CustomArray(const CustomArray& t)
-{
-	this->arr = new T;
-	*arr = t.arr;
+CustomArray<T>::CustomArray(const CustomArray<T> &t) {
+	N = t.N;
+	arr = new T[3 * this->N];
+	for (int i = 0; i < this->N; i++)
+	{
+		*(arr + N + i) = *(t.arr + N + i);
+	}
+
+}
+
+/* Operator = use deep copy */
+template <class T>
+CustomArray<T>& CustomArray<T>::operator=(CustomArray<T> &t) {
+	this->N = t.N;
+	this->arr = new T[3 * this->N];
+	for (int i = 0; i < this->N; i++)
+	{
+		*(arr + N + i) = *(t.arr + N + i);
+	}
+	return *this;
+}
+
+/* Operator [] */
+template <class T>
+T& CustomArray<T>::operator[] (int x) {
+	return *(arr + N + x);
+}
+
+/* Get size of Custom Array */
+template <class T>
+int CustomArray<T>::size() {
+	return N;
 }
 
 template <class T>
-CustomArray<T>& CustomArray<T>::operator=(CustomArray& t)
-{
-	this->arr = new T;
-	*this->arr = *t.arr;
-	return this;
+T CustomArray<T>::begin() {
+	return p_Begin;
 }
+
+template <class T>
+T CustomArray<T>::end() {
+	return p_End;
+}
+
+template <class T>
+void CustomArray<T>::push_back(T element) {
+	if (p_End != p_UpperBound)
+	{
+		*p_End = element;
+		p_End = p_End + 1;
+		N = N + 1;
+	}
+	else
+	{
+		if (p_Begin != p_LowerBound)
+		{
+			p_Begin = p_Begin - 1;
+			for (int i = 0; i < N; i++)
+			{
+				*(p_Begin + i) = *(p_Begin + i + 1);
+			}
+			*p_End = element;
+			N = N + 1;
+		}
+		else
+		{
+			CustomArray<T> newObject(N);
+			for (int i = 0; i < N; i++)
+			{
+				*(newObject.p_Begin + i) = *(p_Begin + i);
+			}
+			*(newObject.p_End) = element;
+			p_End = p_End + 1;
+			N = N + 1;
+			delete this;
+			this = newObject;
+		}
+	}
+}
+
+template <class T>
+void CustomArray<T>::push_front(T element) {
+	if (p_Begin != p_LowerBound)
+	{
+		p_Begin--;
+		*p_Begin = element;
+		N = N + 1;
+	}
+	else
+	{
+		if (p_End != p_UpperBound)
+		{
+			p_End++;
+			for (int i = 0; i < N; i++)
+			{
+				*(p_End - i) = *(p_End - i - 1);
+			}
+			*p_Begin = element;
+			N = N + 1;
+		}
+		else
+		{
+			CustomArray<T> newObject(N);
+			for (int i = 0; i < N; i++)
+			{
+				*(newObject.p_Begin + i) = *(p_Begin + i);
+			}
+			newObject.p_Begin--;
+			*(newObject.p_Begin) = element;
+			N = N + 1;
+			delete this;
+			this = newObject;
+		}
+	}
+}
+
+template <class T>
+T CustomArray<T>::pop_back() {
+	p_End--;
+	return *p_End;
+}
+
+template <class T>
+T CustomArray<T>::pop_front() {
+	T temp = *p_Begin;
+	p_Begin++;
+	return temp;
+}
+
+template <class T>
+void CustomArray<T>::insert(int index) {
+
+}
+
+template <class T>
+void CustomArray<T>::erase(int index) {
+	if (index > ((int) N/2))
+	{
+		for (int i = 0; i < N - index - 1; i++)
+		{
+			*(p_Begin + index + i) = *(p_Begin + index + i + 1);
+		}
+		p_End--;
+		N--;
+	}
+	else
+	{
+		for (int i = 0; i < index; i++)
+		{
+			*(p_Begin + index - i) = *(p_Begin + index - i - 1);
+		}
+		p_Begin++;
+		N--;
+	}
+}
+
+template <class T>
+void CustomArray<T>::sort(bool isIncrease) {
+	for (int i = 0; i < N - 1; i++)
+	{
+		for (int j = i + 1; j < N; j++)
+		{
+			if ((*(begin + i) > *(begin + j) == isIncrease))
+			{
+				T temp = *(begin + i);
+				*(begin + i) = *(begin + j);
+				*(begin + j) = temp;
+			}
+		}
+	}
+}
+
 
--- 1312636/CustomArray.h
+++ 1312636/CustomArray.h
@@ -3,7 +3,7 @@ class CustomArray
 {
 private:
 	int N;
-	T *lowerBound, *begin, *end, *upperBound, *arr;
+	T *p_LowerBound, *p_Begin, *p_End, *p_UpperBound, *arr;
 public:
 	CustomArray(int m_N);
 	CustomArray(int m_N, T defaultValue);
@@ -13,5 +13,19 @@ public:
 	void setDefaultValue(T defaultValue);
 	void printArr();
 	CustomArray& operator=(CustomArray& t);
+	T& operator[] (int x);
+	int size();
+	T begin();
+	T end();
+	void clear();
+	bool empty();
+	void resize();
+	void push_back(T element);
+	void push_front(T element);
+	T pop_back();
+	T pop_front();
+	void insert(int i);
+	void erase(int i);
+	void sort(bool isIncrease);
 };
 
